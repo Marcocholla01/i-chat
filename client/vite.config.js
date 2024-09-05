5cc5a0b81323cba2c040b8850c238056116241ba
Marcocholla01 <marcochollapaul01@gmail.com>
2024-09-05 20:10:08 +0300

    removed dotenv file

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()], //, pluginRewriteAll()
   server: {
-    port: 4000,
+    // port: 4000,
     // proxy: {
     //   "/api": {
     //     target: "http://localhost:5000",
